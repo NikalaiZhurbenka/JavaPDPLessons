30eb345de79b85601b40ecec1248daa9833ed97f
Nikalai Zhurbenka <nikalai.zhurbenka@cogniteq.com>
2023-03-17 00:59:51 +0300

    Added 7.3 task Added part of 4.3

---
+++ src/Line7_3.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Line7_3 {
+    public static void main(String[] args) {
+        System.out.println("Enter any numbers 1 2 3 4 5 12 135 23 ): ");
+        String numbers = new Scanner(System.in).nextLine();
+        Set<String> SetLine = new HashSet<String>();
+        SetLine.addAll(Arrays.asList(numbers.split(" ")));
+        System.out.println(SetLine);
+        //to do - Добавить обработку, что бы выводило только int значения.
+    }
+}
---
+++ src/Matrix.java
@@ -0,0 +1,13 @@
+public class Matrix {
+    double [][] matrix;
+    int rowsQ;
+    int columnsQ;
+
+    public Matrix (int rowsQ, int columnsQ) {
+    matrix = new double[rowsQ][columnsQ];
+    this.rowsQ = rowsQ;
+    this.columnsQ = columnsQ;
+    }
+    
+
+}
