6b20ae7983a3425f6d566c74da400bb93dbe2798
Nikalai Zhurbenka <nikalai.zhurbenka@cogniteq.com>
2023-05-12 17:46:42 +0300

    Fix volatile task Add atomic operation Comparing default/volatile/atomic operations

--- volatileTest/volatilePDP.java
+++ volatileTest/volatilePDP.java
@@ -1,27 +1,48 @@
 package volatileTest;
 
-public class volatilePDP {
-    public  int value = 0;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    public void increaseValue() {
+public class volatilePDP {
+    public static  int value = 0;
+    public static volatile int value_volatile = 0;
+    public static AtomicInteger atomic = new AtomicInteger(0);
+
+    public void increaseValue_default() {
         value++;
+    }
+    public void increaseValue_atomic(){
+        atomic.incrementAndGet();
+    }
+    public void increaseValue_volatile(){
+        value_volatile++;
     }
     public int getValue(){
         return value;
+    }
+    public int getValue_volatile(){
+        return value_volatile;
     }
 
     public static void main(String[] args) throws InterruptedException {
         volatilePDP test = new volatilePDP();
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                test.increaseValue();
-                System.out.println("Counter value1: " + test.getValue());
+            for (int i = 0; i < 1000000; i++) {
+                test.increaseValue_default();
+                System.out.println("Counter value1_default: " + test.getValue());
+                test.increaseValue_atomic();
+                System.out.println("Counter value1_atomic: " + atomic.get());
+                test.increaseValue_volatile();
+                System.out.println("Counter value1_atomic: " + test.getValue_volatile());
             }
         });
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                test.increaseValue();
-                System.out.println("Counter value2: " + test.getValue());
+            for (int n = 0; n < 1000000; n++) {
+                test.increaseValue_default();
+                System.out.println("Counter value2_default: " + test.getValue());
+                test.increaseValue_atomic();
+                System.out.println("Counter value2_atomic: " + atomic.get());
+                test.increaseValue_volatile();
+                System.out.println("Counter value2_atomic: " + test.getValue_volatile());
             }
         });
 
@@ -34,6 +55,8 @@ public class volatilePDP {
         } catch (InterruptedException e){
             e.printStackTrace();
         }
-
+        System.out.println(test.getValue()+"check default");
+        System.out.println(test.getValue_volatile()+"check volatile");
+        System.out.println(atomic.get()+"check atomic");
     }
 }
